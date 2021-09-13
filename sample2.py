af142f4b9787ba2e7f43d91fdd7288673675508c
saksham24sharma <90346325+saksham24sharma@users.noreply.github.com>
2021-09-13 16:08:25 +0530

    Delete sample2.py

--- sample2.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter name: ")
-alphabet = input("Enter an alphabet: ")
-inxd = int(input("Enter index : "))
-new_name = list(name)
-new_name.insert(inxd,alphabet)
-n =""
-for i in new_name:
-    n = n +i
-print(n)
